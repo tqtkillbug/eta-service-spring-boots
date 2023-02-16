9589c42b356b41f569e4d3c6e7ef4bcc10be454b
DESKTOP-GEFTSHN\ADMIN <tqtteams1st@gmail.com>
2023-02-16 22:42:07 +0700

    New model Account

--- src/main/java/net/etaservice/generatoraccount/Account.java
+++ src/main/java/net/etaservice/generatoraccount/Account.java
@@ -1,2 +1,22 @@
-package net.etaservice.generatoraccount;public class Account {
+package net.etaservice.generatoraccount;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class Account {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userName;
+    private String password;
+
+    @Column(name = "is_deleted",columnDefinition = "int default 0", nullable = false)
+    private int isDeleted;
+
+
 }
