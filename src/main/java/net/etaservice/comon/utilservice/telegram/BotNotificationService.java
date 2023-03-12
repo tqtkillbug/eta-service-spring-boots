80d17d8c0d7b996dd83deb518a55337ecc051e03
DESKTOP-GEFTSHN\ADMIN <tqtteams1st@gmail.com>
2023-03-12 23:09:25 +0700

    Remove method unavailble

--- src/main/java/net/etaservice/comon/utilservice/telegram/BotNotificationService.java
+++ src/main/java/net/etaservice/comon/utilservice/telegram/BotNotificationService.java
@@ -141,9 +141,6 @@ public class BotNotificationService {
         botNotification.sendMessge(message);
     }
 
-    public void sendTranferPhoto(SendPhoto sendPhoto){
-        botNotification.sendPhoto(sendPhoto);
-    }
 
     public void handlerCallbackQuery(Update callbackQuery) throws Exception {
         addUpdate(callbackQuery);
